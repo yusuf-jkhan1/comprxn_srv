85d44b213ca9cb04e8c4d9abf12a8b28ecffcd4d
Yusuf Khan <ykhan34@gatech.edu>
2020-12-27 02:02:19 -0500

    fastAPI swagger test, output limit

--- cmprxn/router/cmprxn_router.py
+++ cmprxn/router/cmprxn_router.py
@@ -9,9 +9,10 @@ router = APIRouter()
 def compress_image(path):
     img_utils_obj = img_utils(path)
     my_array = img_utils_obj.reshape_img()
-    cmprxr_obj = k_cluster(array=my_array,k=3, algo_type="means")
-    cmprxr_obj.run()
-    group_assignments = cmprxr_obj.group_assignment_vect[0:5]
-    centroids = cmprxr_obj.current_centers_vect[0:5]
-    response_dict = {"Group Assignments": group_assignments, "Centroids": centroids}   
+    #cmprxr_obj = k_cluster(array=my_array,k=3, algo_type="means")
+    #cmprxr_obj.run()
+    #group_assignments = cmprxr_obj.group_assignment_vect[0:5]
+    #centroids = cmprxr_obj.current_centers_vect[0:5]
+    #response_dict = {"Group Assignments": group_assignments, "Centroids": centroids}
+    response_dict = {"Dims":my_array.shape}   
     return response_dict
