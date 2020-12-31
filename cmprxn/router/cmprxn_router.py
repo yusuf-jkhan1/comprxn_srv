552dc985ecf5382729c430bc781ad8b3c5512a34
Yusuf Khan <ykhan34@gatech.edu>
2020-12-31 04:28:40 -0500

    cmprxn router simplification test

--- cmprxn/router/cmprxn_router.py
+++ cmprxn/router/cmprxn_router.py
@@ -10,7 +10,7 @@ def compress_image(path: str):
     img_utils_obj = img_utils(path)
     my_array = img_utils_obj.reshape_img()
     cmprxr_obj = k_cluster(array=my_array,k=2, algo_type="means")
-    #cmprxr_obj.run()
+    cmprxr_obj.run()
     #group_assignments = cmprxr_obj.group_assignment_vect[0:5]
     #centroids = cmprxr_obj.current_centers_vect[0:5]
     #response_dict = {"Group Assignments": group_assignments, "Centroids": centroids}
