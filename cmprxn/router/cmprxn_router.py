8c154f4806fd671cfee15f6cbb09b3b4f8bf95ac
Yusuf Khan <ykhan34@gatech.edu>
2020-12-31 04:22:31 -0500

    cmprxn router simplification test

--- cmprxn/router/cmprxn_router.py
+++ cmprxn/router/cmprxn_router.py
@@ -14,6 +14,7 @@ def compress_image(path: str):
     #group_assignments = cmprxr_obj.group_assignment_vect[0:5]
     #centroids = cmprxr_obj.current_centers_vect[0:5]
     #response_dict = {"Group Assignments": group_assignments, "Centroids": centroids}
-    response_dict = {"k": cmprxr_obj.k, "algo": cmprxr_obj.algo_type, "Initial Centers": cmprxr_obj.initial_centers_vect}   
+    response_dict = {"Dims": my_array.shape}
+    #response_dict = {"k": cmprxr_obj.k, "algo": cmprxr_obj.algo_type, "Initial Centers": cmprxr_obj.initial_centers_vect}   
     return response_dict
 
