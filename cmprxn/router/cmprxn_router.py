5281ff752346f13b1fac5637f6412503656b2dac
Yusuf Khan <ykhan34@gatech.edu>
2021-01-03 15:04:14 -0500

    cmprxn router response dtype changes

--- cmprxn/router/cmprxn_router.py
+++ cmprxn/router/cmprxn_router.py
@@ -12,10 +12,8 @@ def compress_image(path: str):
     my_array = img_utils_obj.reshape_img()
     cmprxr_obj = k_cluster(array=my_array,k=2, algo_type="means")
     cmprxr_obj.run()
-    #group_assignments = cmprxr_obj.group_assignment_vect[0:5]
-    #centroids = cmprxr_obj.current_centers_vect[0:5]
-    #array_head = JSONResponse(my_array[0:5])
-    my_array_str = cmprxr_obj.group_assignment_vect.tolist()
-    response_dict = {"Dim": ['a','b','c'], "Head": my_array_str}  
+    group_assignments = cmprxr_obj.group_assignment_vect.tolist()
+    centroids = cmprxr_obj.current_centers_vect.tolist()
+    response_dict = {"Centroids": centroids, "Labels": group_assignments}  
     return response_dict
 
